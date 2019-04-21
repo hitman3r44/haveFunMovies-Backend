208d1fd8df110c202aa78c1dbf1d11777a3c5963
Mehedi Hasan <mehedi.thedue@gmail.com>
2019-04-21 15:29:23 +0600

    HFM-87 Configure Laravel Mix

--- resources/views/admin/moderators/moderators.blade.php
+++ resources/views/admin/moderators/moderators.blade.php
@@ -1,169 +1,199 @@
-@extends('layouts.admin')
+{{--@extends('layouts.admin')--}}
+@extends('layouts.adminator.master')
 
 @section('title', tr('moderators'))
 
-@section('content-header', tr('moderators'))
+@section('content-header')
+    <h4 class="c-grey-900 mT-10 mB-30"> {{ tr('add_moderator') }}</h4>
+@endsection
 
 @section('breadcrumb')
-	<li><a href="{{route('admin.dashboard')}}"><i class="fa fa-dashboard"></i>{{tr('home')}}</a></li>
-    
-    <li class="active"><i class="fa fa-users"></i> {{tr('moderators')}}</li>
-
+    <li class="list-inline-item"><a href="{{route('admin.dashboard')}}"><i class="fa fa-dashboard"></i>{{tr('home')}}
+        </a> >
+    </li>
+    <li class="list-inline-item active"><i class="fa fa-users"></i> {{tr('moderators')}}</li>
 @endsection
 
 @section('content')
 
-	@include('notification.notify')
+    @include('notification.notify')
 
-	<div class="row">
+    <div class="row gap-20">
+        <div class="col-md-12">
+            <div class="bgc-white p-20 bd">
 
-        <div class="col-xs-12">
+                <div class="row bgc-grey-600 p-10">
 
-          	<div class="box box-primary">
-          	
-	          	<div class="box-header label-primary">
-	                <b style="font-size:18px;">{{tr('moderators')}}</b>
-	                <a href="{{route('admin.add.moderator')}}" class="btn btn-default pull-right">{{tr('add_moderator')}}</a>
+                    <div class="col-md-6 text-white">
+                        <h3>{{tr('add_moderator')}}</h3>
+                    </div>
 
-	                <!-- EXPORT OPTION START -->
+                    <div class="col-md-6">
+                        <a href="{{route('admin.add.moderator')}}"
+                           class="btn btn-default pull-right">{{tr('moderators')}}</a>
 
-					@if(count($moderators) > 0 )
-	                
-		                <ul class="admin-action btn btn-default pull-right" style="margin-right: 20px">
-		                 	
-							<li class="dropdown">
-				                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-				                  {{tr('export')}} <span class="caret"></span>
-				                </a>
-				                <ul class="dropdown-menu">
-				                  	<li role="presentation">
-				                  		<a role="menuitem" tabindex="-1" href="{{route('admin.moderators.export' , ['format' => 'xls'])}}">
-				                  			<span class="text-red"><b>{{tr('excel_sheet')}}</b></span>
-				                  		</a>
-				                  	</li>
+                        @if(count($moderators) > 0 )
 
-				                  	<li role="presentation">
-				                  		<a role="menuitem" tabindex="-1" href="{{route('admin.moderators.export' , ['format' => 'csv'])}}">
-				                  			<span class="text-blue"><b>{{tr('csv')}}</b></span>
-				                  		</a>
-				                  	</li>
-				                </ul>
-							</li>
-						</ul>
+                            <ul class="admin-action btn btn-default pull-right" style="margin-right: 20px">
 
-					@endif
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                        {{tr('export')}} <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li role="presentation">
+                                            <a role="menuitem" tabindex="-1"
+                                               href="{{route('admin.moderators.export' , ['format' => 'xls'])}}">
+                                                <span class="text-red"><b>{{tr('excel_sheet')}}</b></span>
+                                            </a>
+                                        </li>
 
-	                <!-- EXPORT OPTION END -->
-	                
-	            </div>
-	            
-	            <div class="box-body">
+                                        <li role="presentation">
+                                            <a role="menuitem" tabindex="-1"
+                                               href="{{route('admin.moderators.export' , ['format' => 'csv'])}}">
+                                                <span class="text-blue"><b>{{tr('csv')}}</b></span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
 
-	            	@if(count($moderators) > 0)
+                        @endif
+                    </div>
+                </div>
+                <div class="box-body">
 
-		              	<table id="datatable-withoutpagination" class="table table-bordered table-striped">
+                    @if(count($moderators) > 0)
 
-							<thead>
-							    <tr>
-							      <th>{{tr('id')}}</th>
-							      <th>{{tr('username')}}</th>
-							      <th>{{tr('email')}}</th>
-							      <th>{{tr('mobile')}}</th>
-							      <th>{{tr('address')}}</th>
-							      <th>{{tr('total_videos')}}</th>
-							      <th>{{tr('total')}}</th>
-							      <th>{{tr('status')}}</th>
-							      <th>{{tr('action')}}</th>
-							    </tr>
-							</thead>
+                        <div class="table table-responsive">
 
-							<tbody>
+                            <table id="dataTable" class="table table-striped table-bordered" cellspacing="0"
+                                   width="100%">
 
-								@foreach($moderators as $i => $moderator)
+                                <thead>
+                                <tr>
+                                    <th>{{tr('id')}}</th>
+                                    <th>{{tr('username')}}</th>
+                                    <th>{{tr('email')}}</th>
+                                    <th>{{tr('mobile')}}</th>
+                                    <th>{{tr('address')}}</th>
+                                    <th>{{tr('total_videos')}}</th>
+                                    <th>{{tr('total')}}</th>
+                                    <th>{{tr('status')}}</th>
+                                    <th>{{tr('action')}}</th>
+                                </tr>
+                                </thead>
 
-								    <tr>
-								      <td>{{showEntries($_GET, $i+1)}}</td>
-								      <td><a href="{{route('admin.moderator.view',$moderator->id)}}">{{$moderator->name}}</a></td>
-								      <td>{{$moderator->email}}</td>
-								      <td>{{$moderator->mobile}}</td>
-								      <td>{{$moderator->address}}</td>
-								      <td><a href="{{route('admin.moderator.videos.list',$moderator->id)}}" >{{$moderator->moderatorVideos ? $moderator->moderatorVideos->count() : "0.00"}}</a></td>
-								      <td>{{Setting::get('currency')}} {{$moderator->total}}</td>
+                                <tbody>
 
-								      <td>
-								      		@if($moderator->is_activated)
-								      			<span class="label label-success">{{tr('approved')}}</span>
-								       		@else
-								       			<span class="label label-warning">{{tr('pending')}}</span>
-								       		@endif
-								       </td>
-								      <td>
-	            							<ul class="admin-action btn btn-default">
-	            								<li class="dropup">
-									                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-									                  {{tr('action')}} <span class="caret"></span>
-									                </a>
-									                <ul class="dropdown-menu dropdown-menu-right">
-									                	
-									                  	<li role="presentation">
-															@if(Setting::get('admin_delete_control'))
-	                                                            <a role="button" href="javascript:;" class="btn disabled" style="text-align: left">{{tr('edit')}}</a>
-	                                                        @else
-																<a role="menuitem" tabindex="-1" href="{{route('admin.edit.moderator',$moderator->id)}}">{{tr('edit')}}</a>
-															@endif
-														</li>
+                                @foreach($moderators as $i => $moderator)
 
-									                  	<li role="presentation"><a role="menuitem" tabindex="-1" href="{{route('admin.moderator.view',$moderator->id)}}">{{tr('view')}}</a></li>
-									                  
-									                  	<li role="presentation" class="divider"></li>
-									                  	@if($moderator->is_activated)
-									                		<li role="presentation"><a role="menuitem" tabindex="-1" onclick="return confirm(&quot;{{tr('moderator_decline_confirmation' , $moderator->name)}}&quot;);" href="{{route('admin.moderator.decline',$moderator->id)}}">{{tr('decline')}}</a></li>
-									                	@else
-									                  		<li role="presentation"><a role="menuitem" tabindex="-1" onclick="return confirm(&quot;{{tr('moderator_approve_confirmation' , $moderator->name)}}&quot;);"  href="{{route('admin.moderator.approve',$moderator->id)}}">{{tr('approve')}}</a></li>
-									                  	@endif
+                                    <tr>
+                                        <td>{{showEntries($_GET, $i+1)}}</td>
+                                        <td>
+                                            <a href="{{route('admin.moderator.view',$moderator->id)}}">{{$moderator->name}}</a>
+                                        </td>
+                                        <td>{{$moderator->email}}</td>
+                                        <td>{{$moderator->mobile}}</td>
+                                        <td>{{$moderator->address}}</td>
+                                        <td>
+                                            <a href="{{route('admin.moderator.videos.list',$moderator->id)}}">{{$moderator->moderatorVideos ? $moderator->moderatorVideos->count() : "0.00"}}</a>
+                                        </td>
+                                        <td>{{Setting::get('currency')}} {{$moderator->total}}</td>
 
-									                  	<li role="presentation">
+                                        <td>
+                                            @if($moderator->is_activated)
+                                                <span class="badge badge-success">{{tr('approved')}}</span>
+                                            @else
+                                                <span class="badge badge-warning">{{tr('pending')}}</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <ul class="admin-action btn btn-default">
+                                                <li class="dropdown">
+                                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                                        {{tr('action')}} <span class="caret"></span>
+                                                    </a>
+                                                    <ul class="dropdown-menu dropdown-menu-right">
 
-									                  		@if(Setting::get('admin_delete_control'))
+                                                        <li role="presentation">
+                                                            @if(Setting::get('admin_delete_control'))
+                                                                <a role="button" href="javascript:;"
+                                                                   class="btn disabled"
+                                                                   style="text-align: left">{{tr('edit')}}</a>
+                                                            @else
+                                                                <a role="menuitem" tabindex="-1"
+                                                                   href="{{route('admin.edit.moderator',$moderator->id)}}">{{tr('edit')}}</a>
+                                                            @endif
+                                                        </li>
 
-										                  	 	<a role="button" href="javascript:;" class="btn disabled" style="text-align: left">{{tr('delete')}}</a>
+                                                        <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                                                   href="{{route('admin.moderator.view',$moderator->id)}}">{{tr('view')}}</a>
+                                                        </li>
 
-										                  	 @else
+                                                        <li role="presentation" class="divider"></li>
+                                                        @if($moderator->is_activated)
+                                                            <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                                                       onclick="return confirm(&quot;{{tr('moderator_decline_confirmation' , $moderator->name)}}&quot;);"
+                                                                                       href="{{route('admin.moderator.decline',$moderator->id)}}">{{tr('decline')}}</a>
+                                                            </li>
+                                                        @else
+                                                            <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                                                       onclick="return confirm(&quot;{{tr('moderator_approve_confirmation' , $moderator->name)}}&quot;);"
+                                                                                       href="{{route('admin.moderator.approve',$moderator->id)}}">{{tr('approve')}}</a>
+                                                            </li>
+                                                        @endif
 
-									                  			<a role="menuitem" tabindex="-1" onclick="return confirm(&quot;{{tr('admin_moderator_delete_confirmation' , $moderator->name)}}&quot;);" href="{{route('admin.delete.moderator', $moderator->id)}}">{{tr('delete')}}</a>
-									                  		@endif
+                                                        <li role="presentation">
 
-									                  	</li>
-									                  	
-									                  	<li role="presentation" class="divider"></li>
+                                                            @if(Setting::get('admin_delete_control'))
 
-									                  	<li role="presentation"><a role="menuitem" tabindex="-1" href="{{route('admin.moderators.redeems',['id'=>$moderator->id])}}">{{tr('redeems')}}</a></li>
+                                                                <a role="button" href="javascript:;"
+                                                                   class="btn disabled"
+                                                                   style="text-align: left">{{tr('delete')}}</a>
 
-									                  	<li>
+                                                            @else
 
-									                  	<li role="presentation">
-									                  		<a role="menuitem" tabindex="-1" href="{{route('admin.videos' , array('moderator_id' => $moderator->id))}}">{{tr('videos')}}</a>
-									                  	</li>
+                                                                <a role="menuitem" tabindex="-1"
+                                                                   onclick="return confirm(&quot;{{tr('admin_moderator_delete_confirmation' , $moderator->name)}}&quot;);"
+                                                                   href="{{route('admin.delete.moderator', $moderator->id)}}">{{tr('delete')}}</a>
+                                                            @endif
 
-									                  	
-									                </ul>
-	              								</li>
-	            							</ul>
-								      </td>
-								    </tr>
-								@endforeach
-							</tbody>
-						
-						</table>
+                                                        </li>
 
-						<div align="right" id="paglink"><?php echo $moderators->links(); ?></div>
-					@else
-						<h3 class="no-result">{{tr('no_result_found')}}</h3>
-					@endif
-	            
-	            </div>
-          	
-          	</div>
+                                                        <li role="presentation" class="divider"></li>
+
+                                                        <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                                                   href="{{route('admin.moderators.redeems',['id'=>$moderator->id])}}">{{tr('redeems')}}</a>
+                                                        </li>
+
+                                                        <li>
+
+                                                        <li role="presentation">
+                                                            <a role="menuitem" tabindex="-1"
+                                                               href="{{route('admin.videos' , array('moderator_id' => $moderator->id))}}">{{tr('videos')}}</a>
+                                                        </li>
+
+
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+
+                            </table>
+
+                        </div>
+                        <div align="right" id="paglink"><?php echo $moderators->links(); ?></div>
+                    @else
+                        <h3 class="no-result">{{tr('no_result_found')}}</h3>
+                    @endif
+
+                </div>
+
+            </div>
 
         </div>
     </div>
--- resources/views/admin/users/add-user.blade.php
+++ resources/views/admin/users/add-user.blade.php
@@ -3,25 +3,31 @@
 @section('title', tr('add_user'))
 
 @section('content-header')
-    <h4 class="c-grey-900 mT-10 mB-30">{{tr('add_user')}}</h4>
+    <h4 class="c-grey-900 mT-10 mB-30"> {{ tr('add_user') }}</h4>
 @endsection
-
+@section('breadcrumb')
+    <li class="list-inline-item"><a href="{{route('admin.dashboard')}}"><i class="fa fa-dashboard"></i>{{tr('home')}}
+        </a></li>
+    <li class="list-inline-item"><a href="{{route('admin.users')}}"><i class="fa fa-user"></i> {{tr('users')}}</a></li>
+    <li class="list-inline-item"><i class="fa fa-user"></i> {{tr('view_user')}}</li>
+@endsection
 
 @section('content')
 
     <div class="row gap-20">
         <div class="col-md-10">
             <div class="bgc-white p-20 bd">
-                <div class="row bgc-green-300 p-10">
+                <div class="row bgc-grey-400 p-10">
                     <div class="col-8">
-                        <h6 class="c-grey-900">{{tr('add_user')}}</h6>
+                        <h6 class="c-grey-900"><b>{{tr('add_user')}}</b></h6>
                     </div>
                     <div class="col-4">
                         <a href="{{route('admin.users')}}" class="btn btn-primary pull-right">{{tr('view_users')}}</a>
                     </div>
                 </div>
 
-                <form class="form-horizontal" action="{{route('admin.save.user')}}" method="POST" enctype="multipart/form-data" role="form">
+                <form class="form-horizontal" action="{{route('admin.save.user')}}" method="POST"
+                      enctype="multipart/form-data" role="form">
 
                     <div class="mT-30">
 
@@ -30,14 +36,18 @@
 
                             <div class="col-sm-10">
                                 <input type="text" required name="name"
-                                pattern = "[a-zA-Z,0-9\s\-\.]{2,100}" title="{{tr('only_alphanumeric')}}" class="form-control" id="username" placeholder="{{tr('name')}}" value="{{old('name')}}">
+                                       pattern="[a-zA-Z,0-9\s\-\.]{2,100}" title="{{tr('only_alphanumeric')}}"
+                                       class="form-control" id="username" placeholder="{{tr('name')}}"
+                                       value="{{old('name')}}">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-sm-2 control-label">* {{tr('email')}}</label>
                             <div class="col-sm-10">
-                                <input type="email" maxlength="255" required class="form-control" id="email" name="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,10}$" placeholder="{{tr('email')}}" value="{{old('email')}}">
+                                <input type="email" maxlength="255" required class="form-control" id="email"
+                                       name="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,10}$"
+                                       placeholder="{{tr('email')}}" value="{{old('email')}}">
                             </div>
                         </div>
 
@@ -45,8 +55,10 @@
                             <label for="mobile" class="col-sm-2 control-label">* {{tr('mobile')}}</label>
 
                             <div class="col-sm-10">
-                                <input type="text" required name="mobile" class="form-control" id="mobile" placeholder="{{tr('mobile')}}" minlength="4" maxlength="16" pattern="[0-9]{4,16}" value="{{old('mobile')}}">
-                                 <small style="color:brown">{{tr('mobile_note')}}</small>
+                                <input type="text" required name="mobile" class="form-control" id="mobile"
+                                       placeholder="{{tr('mobile')}}" minlength="4" maxlength="16" pattern="[0-9]{4,16}"
+                                       value="{{old('mobile')}}">
+                                <small style="color:brown">{{tr('mobile_note')}}</small>
                             </div>
                         </div>
 
@@ -54,15 +66,21 @@
                             <label for="password" class="col-sm-2 control-label">* {{tr('password')}}</label>
 
                             <div class="col-sm-10">
-                                <input type="password" required  name="password" pattern=".{6,}" title="{{tr('password_notes')}}" class="form-control" id="password" placeholder="{{tr('password')}}" value="{{old('password')}}">
+                                <input type="password" required name="password" pattern=".{6,}"
+                                       title="{{tr('password_notes')}}" class="form-control" id="password"
+                                       placeholder="{{tr('password')}}" value="{{old('password')}}">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="username" class="col-sm-2  control-label">* {{tr('password_confirmation')}}</label>
+                            <label for="username"
+                                   class="col-sm-2  control-label">* {{tr('password_confirmation')}}</label>
 
                             <div class="col-sm-10">
-                                <input type="password" required pattern=".{6,}"  title="{{tr('password_notes')}}"  name="password_confirmation" class="form-control" id="username" placeholder="{{tr('password_confirmation')}}"  value="{{old('password_confirmation')}}">
+                                <input type="password" required pattern=".{6,}" title="{{tr('password_notes')}}"
+                                       name="password_confirmation" class="form-control"
+                                       placeholder="{{tr('password_confirmation')}}"
+                                       value="{{old('password_confirmation')}}">
                             </div>
                         </div>
 
@@ -87,17 +105,17 @@
 
 @section('scripts')
 
-<script src="{{asset('assets/js/jstz.min.js')}}"></script>
-<script>
+    <script src="{{asset('assets/js/jstz.min.js')}}"></script>
+    <script>
 
-    $(document).ready(function() {
+        $(document).ready(function () {
 
-        var dMin = new Date().getTimezoneOffset();
-        var dtz = -(dMin/60);
-        // alert(dtz);
-        $("#userTimezone").val(jstz.determine().name());
-    });
+            var dMin = new Date().getTimezoneOffset();
+            var dtz = -(dMin / 60);
+            // alert(dtz);
+            $("#userTimezone").val(jstz.determine().name());
+        });
 
-</script>
+    </script>
 
 @endsection
