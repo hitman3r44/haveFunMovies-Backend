50e46c29c63fdd9441a82d904785d2b0f8e91244
hitman3r44 <hitman3r44@gmail.com>
2019-05-31 06:24:54 -0400

    - Cast_and_crews
    - Add "appzcoder/crud-generator --dev" packages

---
+++ app/CastAndCrewType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CastAndCrewType extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/CastAndCrewTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CastAndCrewType;
+use Illuminate\Http\Request;
+
+class CastAndCrewTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CastAndCrewType  $castAndCrewType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CastAndCrewType $castAndCrewType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CastAndCrewType  $castAndCrewType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CastAndCrewType $castAndCrewType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CastAndCrewType  $castAndCrewType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CastAndCrewType $castAndCrewType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CastAndCrewType  $castAndCrewType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CastAndCrewType $castAndCrewType)
+    {
+        //
+    }
+}
