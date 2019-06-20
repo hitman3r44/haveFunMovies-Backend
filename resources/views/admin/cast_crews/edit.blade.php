7fa5a131413c2aca5c49284c3cf5dc6d0835168c
mehedithedue <mehedi.thedue@gmail.com>
2019-06-20 20:57:06 +0000

    HFM-120 cast and crew donw

--- resources/views/admin/cast_crews/edit.blade.php
+++ resources/views/admin/cast_crews/edit.blade.php
@@ -17,6 +17,38 @@
 
 @include('admin.cast_crews._form')
 
+
+
+<div class="modal fade" id="add_cast_type_modal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Cast Crew Type </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Type Name:</label>
+                        <input type="text" class="form-control" name="title" id="type_name">
+                    </div>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Type Description:</label>
+                        <textarea class="form-control" name="type_description" id="type_description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success">Save Type</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 @endsection
 
 @section('scripts')
@@ -41,7 +73,85 @@
     </script>
 
     <script src="https://cdn.ckeditor.com/4.5.5/standard/ckeditor.js"></script>
+
     <script>
-        CKEDITOR.replace( 'ckeditor' );
+        CKEDITOR.replace('ckeditor');
+    </script>
+
+    <script src="{{asset('assets/js/jstz.min.js')}}"></script>
+    <script>
+        function populateSelectOptionData(contentType, viewField) {
+            $.ajax({
+                type: "GET",
+                url: "{{ route('admin.cast-and-crew-type.data') }}",
+                data: {
+                    content: contentType
+                },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': "{{csrf_token()}}"
+                },
+                success: function (response) {
+                    var option = '';
+                    if (response.statusCode === 1) {
+
+                        $.each(response.data, function (id, value) {
+                            option += '<option value="' + id + '" >' + value + '</option>';
+                        });
+
+                        $(viewField).html(option);
+
+                        $(viewField).val(parseInt({{$model->cast_and_crew_type_id}}));
+                        $(viewField).trigger('change.select2');
+
+
+                    } else {
+                        alert(response.message);
+                    }
+                }
+            });
+        }
+
+
+        function addNewCastType(data) {
+            $.ajax({
+                type: "POST",
+                url: "{{ route('admin.cast_and_crew_type.store') }}",
+                data: data,
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': "{{csrf_token()}}"
+                },
+                success: function (response) {
+                    if (response.statusCode === 1) {
+
+                        populateSelectOptionData('cast-and-crews-types', '#cast-and-crews-types');
+                    }else{
+                        alert(response.message);
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(jqXHR.responseJSON.errors.title[0]);
+                }
+            });
+        }
+
+        $(document).ready(function () {
+            var dMin = new Date().getTimezoneOffset();
+            var dtz = -(dMin / 60);
+            $("#userTimezone").val(jstz.determine().name());
+
+            populateSelectOptionData('cast-and-crews-types', '#cast-and-crews-types');
+        });
+
+
+        $('.modal#add_cast_type_modal form').on('submit', function (event) {
+
+            $('#add_cast_type_modal').modal('hide');
+            event.preventDefault();
+            addNewCastType($(this).serialize());
+        })
+
+
     </script>
 @endsection
